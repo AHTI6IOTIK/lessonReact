bf052705db36edbe30c13027e991abe8cdb26693
frimen <myEmail@email.ru>
2018-08-04 23:08:18 +0300

    webpack.config && package.json

--- home_work_3/webpack.config.js
+++ home_work_3/webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const ENTRY_FILE_PATH = path.resolve(__dirname, 'src', 'index.js')
+const ENTRY_PATH = path.resolve(__dirname, 'src')
 const OUTPUT_PATH = path.resolve(__dirname, 'dist')
 
 module.exports = {
-    entry: ENTRY_FILE_PATH,
-    devServer: {
-        port: 3030,
-        contentBase: OUTPUT_PATH,
-        open: true
-    },
+    mode: 'production',
+    entry: [ 'babel-polyfill', path.resolve(ENTRY_PATH, 'index.js') ],
     output: {
         path: OUTPUT_PATH,
         filename: 'bundle.js'
     },
+    devServer: {
+        port: 3030,
+        contentBase: OUTPUT_PATH,
+        compress: true,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(ENTRY_PATH, 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
@@ -22,9 +30,13 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'react', 'stage-2']
-                    }
+                        presets: [ 'env', 'stage-2', 'react' ]
+                    },
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
             }
         ]
     }
